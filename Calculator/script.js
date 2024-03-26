26d9c9527b8bff52f01a1c32396b58b5dbfe5cab
Zodik09 <adarshvishwakarma09nov2k@gmail.com>
2024-03-26 19:42:33 +0530

    Added the media query and comments in the style.css file and comments in the index.html or script.js file

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,23 +1,32 @@
+// Variable btn to store the object of all the buttons
 let btn = document.querySelectorAll("button");
+// Variable input to store the object of result
 let input = document.getElementById("result");
 
+// Empty string to add the values of the result
 let string = "";
+// Creating array from the btn variable
 let arr = Array.from(btn);
+
 arr.forEach(button => {
     button.addEventListener("click", (e) => {
-        if(e.target.innerHTML == "="){
+        // On clicking = button evaluate the display data.
+        if (e.target.innerHTML == "=") {
             string = eval(string);
             input.value = string;
         }
-        else if(e.target.innerHTML == "AC"){
+        // On clicking AC button clear the display.
+        else if (e.target.innerHTML == "AC") {
             string = "";
             input.value = string;
         }
-        else if(e.target.innerHTML == "DEL"){
-            string = string.substring(0, string.length-1);
+        // On clicking DEL button delete the last data on the  display.
+        else if (e.target.innerHTML == "DEL") {
+            string = string.substring(0, string.length - 1);
             input.value = string;
         }
-        else{
+        // On click any other button update the empty string and display it.
+        else {
             string += e.target.innerHTML;
             input.value = string;
         }
